3bf638b716667699123d78d3d0b363e7cdbd0727
Aidas <aidasv@gmail.com>
2017-07-19 18:16:58 +0300

    improvements to gallery view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   def index
     if session[:search_tag].present?
       @photos = flickr.photos.search(tags: session[:search_tag], per_page: 500).
-                to_a.paginate(page: params[:page], per_page: 10)
+                to_a.in_groups_of(2).paginate(page: params[:page], per_page: 10)
       respond_to do |format|
         format.html
         format.js
